5dc20d9befbfbeec26f37e358d095a4040099a9b
xPlorinRolyPoly <alpanachaphalkar@gmail.com>
2021-01-23 16:34:32 +0100

    data-sources section added.

---
+++ Configurations/data-sources/data-source.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "eu-west-2"
+}
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners = ["amazon"]
+
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "instance-1" {
+    ami = data.aws_ami.app_ami.id
+    instance_type = "t2.micro"
+}
