75c07dccd66988d2076a47ef2d2912f5dcf9de81
D066624 <alpana.chaphalkar@sap.com>
2021-01-23 10:31:01 +0100

    init

---
+++ Congiruations/attributes.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "eu-central-1"
+  access_key = "AKIAT3RALHNNRWTLFKFZ"
+  secret_key = "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"
+}
+
+resource "aws_eip" "lb" {
+  vpc = true
+}
+
+resource "aws_s3_bucket" "terraforms3" {
+  bucket = "alpana-terraform-bkt"
+}
+
+/* output "eip" {
+  value = aws_eip.lb
+}
+
+output "terraforms3bkt" {
+  value = aws_s3_bucket.terraforms3
+} */
---
+++ introduction/first_ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "eu-central-1"
+  access_key = "AKIAT3RALHNNRWTLFKFZ"
+  secret_key = "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"
+}
+
+/* resource "aws_instance" "ec2_terraform" {
+  ami = "ami-03c3a7e4263fd998c"
+  instance_type = "t2.micro"
+}  */
